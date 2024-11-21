d236c865da897d137552065e1af93635858f9173
Ahmad Mirzaei <a.mirzaei.dev@gmail.com>
2024-11-21 21:49:26 +0100

    feat: dockerize publisher app and introduce mongodb and rabbit mq

--- pointCloudPublisher/src/config/index.ts
+++ pointCloudPublisher/src/config/index.ts
@@ -1,1 +1,3 @@
 export const SERVER_PORT = process.env.SERVER_PORT;
+// mongodb://root:example@localhost:27017/point-cloud-storage
+export const MONGO_URI = `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.DB_NAME}`;
--- pointCloudPublisher/src/index.ts
+++ pointCloudPublisher/src/index.ts
@@ -2,9 +2,12 @@ require("dotenv").config();
 import express from "express";
 import {SERVER_PORT} from "./config";
 import App from "./services/ExpressApp";
+import DatabaseConnection from "./services/Database";
 
 const startServer = async () => {
   const app = express();
+
+  await DatabaseConnection();
 
   await App(app);
 
