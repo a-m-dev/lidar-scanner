4e2dcacf0475192ba5364b71eeb38dedabb6d484
Ahmad Mirzaei <a.mirzaei.dev@gmail.com>
2024-11-24 17:33:25 +0100

    chore: css styling changes

--- pointCloudVisualizer/src/components/SessionsList/SessionsList.tsx
+++ pointCloudVisualizer/src/components/SessionsList/SessionsList.tsx
@@ -9,16 +9,21 @@ const SessionsList = () => {
     <div className="sessions-list">
       <div className="container">
         <div className="title">
-          <h1>Available Sessions:</h1>
+          <div className="right">
+            <div className="logo">
+              <img src="https://logosandtypes.com/wp-content/uploads/2024/01/qlik.svg" />
+            </div>
+            <h1>Available Sessions:</h1>
+          </div>
           <button onClick={reload}>Reload</button>
         </div>
 
         <div className="list-wrapper">
           {isLoading && <h3>Loading...</h3>}
 
-          {!isLoading && data.length === 0 ? (
-            <h3>No Session is available!</h3>
-          ) : (
+          {!isLoading && data.length === 0 && <h3>No Session is available!</h3>}
+
+          {!isLoading && data.length > 0 && (
             <div className="grid-wrapper">
               {data.map((s) => (
                 <SessionComponent key={s._id} session={s} />
