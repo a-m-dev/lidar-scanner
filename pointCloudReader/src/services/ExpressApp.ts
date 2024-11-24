bf01fe9e5c9961736518ba45dcf9be00d43ed992
Ahmad Mirzaei <a.mirzaei.dev@gmail.com>
2024-11-24 09:21:12 +0100

    feat: paginated reader api

--- pointCloudReader/src/services/ExpressApp.ts
+++ pointCloudReader/src/services/ExpressApp.ts
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 import {SessionSchema} from "../models/Sessions";
 import cors from "cors";
 
+type RequestBatchQueryParams = {
+  page: number;
+  batchSize: number;
+};
+
 export default async (app: Application) => {
   app.use(cors());
   app.use(express.json({limit: "50mb"}));
@@ -15,16 +20,32 @@ export default async (app: Application) => {
   app.get("/session/:sessionId", async (req, res) => {
     try {
       const {sessionId} = req.params;
+      const {page = 1, batchSize = 5000} =
+        req.query as unknown as RequestBatchQueryParams;
+
+      const offset = (page - 1) * batchSize;
       const model = mongoose.model(sessionId, SessionSchema);
-      const result = await model.find();
-      // .skip(50 * 1000)
-      // .limit(5000);
+      const result = await model.find().skip(offset).limit(batchSize);
 
       return res.status(200).json({
-        message: "Data collected",
+        message: `Data collected for page ${page}`,
         size: result.length,
+        hasNextPage: result.length === batchSize,
         data: result,
       });
+
+      // TODO: in front end, you call it like:
+      // let page = 1;
+      // let hasMore = true;
+
+      // while (hasMore) {
+      //   const response = await fetch(`/data?page=${page}`);
+      //   const {data, hasNextPage} = await response.json();
+
+      //   console.log(data); // Process data
+      //   hasMore = hasNextPage;
+      //   page++;
+      // }
     } catch (error) {
       console.log(error);
       console.log(`[Reader]: failed to get data`);
